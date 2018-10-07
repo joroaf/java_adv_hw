5157a75acf2fa582f3a9a53a7283dd67f2a039d1
G. Kostadinov <Kostadinov.GG@gmail.com>
2018-10-07 12:02:26 +0300

    Zad1 completed

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -1,8 +1,37 @@
 package hw1;
 
+import hw1.zad1.CircularArray;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    @Test
+    public void testCircularArray() {
+        CircularArray<Integer> circularArray = new CircularArray<>(6);
+
+        circularArray.add(1);
+        circularArray.addLast(2);
+        circularArray.add(3);
+        circularArray.addFirst(0);
+        circularArray.addLast(4);
+        circularArray.addLast(5);
+
+        Assert.assertArrayEquals(new Integer[]{0, 1, 2, 3, 4, 5}, circularArray.toArray());
+        Assert.assertEquals(6, circularArray.size());
+
+        Assert.assertEquals(new Integer(1), circularArray.get(7));
+        Assert.assertEquals(new Integer(0), circularArray.getFirst());
+        Assert.assertEquals(new Integer(5), circularArray.getLast());
+
+        Assert.assertEquals(new Integer(0), circularArray.popFirst());
+        Assert.assertEquals(new Integer(5), circularArray.popLast());
+
+        Assert.assertArrayEquals(new Integer[]{1, 2, 3, 4}, circularArray.toArray());
+        Assert.assertEquals(4, circularArray.size());
     }
 }
--- src/hw1/zad1/CircularArray.java
+++ src/hw1/zad1/CircularArray.java
@@ -1,4 +1,69 @@
 package hw1.zad1;
 
-public class CircularArray {
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CircularArray<T> extends ArrayList<T> {
+
+    public CircularArray() {
+        super();
+    }
+
+    public CircularArray(int initCapacity) {
+        super(initCapacity);
+    }
+
+    public CircularArray(Collection<? extends T> c) {
+        super(c);
+    }
+
+    public void addFirst(T e) {
+        add(0, e);
+    }
+
+    public void addLast(T e) {
+        add(e);
+    }
+
+    @Override
+    public T get(int n) {
+        return super.get(n % size());
+    }
+
+    public T getFirst() {
+        return super.get(0);
+    }
+
+    public T getLast() {
+        return super.get(size() - 1);
+    }
+
+    public T popFirst() {
+        return remove(0);
+    }
+
+    public T popLast() {
+        return remove(size() - 1);
+    }
+
+    public void removeFromEnd(int numOfElements) {
+        if (numOfElements > size()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        for (int i = 1; i <= numOfElements; i++) {
+            remove(size() - i);
+        }
+    }
+
+    public void removeFromStart(int numOfElements) {
+        if (numOfElements > size()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        for (int i = 0; i < numOfElements; i++) {
+            remove(i);
+        }
+    }
 }
